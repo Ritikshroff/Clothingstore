230e938c24b971bc7125d5ea4008951a4759ebeb
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2025-01-24 22:12:04 +0530

    project created

---
+++ Syncmarket.in/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 class="text-3xl font-bold text-black underline align-item-centre">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
