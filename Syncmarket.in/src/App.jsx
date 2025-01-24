83effb8500b7eb58e84b3a4c2c636f863a09b9ed
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2025-01-24 22:20:20 +0530

    setup project

--- Syncmarket.in/src/App.jsx
+++ Syncmarket.in/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <>
-     <h1 class="text-3xl font-bold text-black underline align-item-centre">
-      Hello world!
-    </h1>
+      <h1 class="text-3xl font-bold text-black underline align-item-centre">
+        Syncmarket.in
+      </h1>
     </>
   )
 }
